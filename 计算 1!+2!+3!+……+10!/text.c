2dac07b612665d8661c4a22ea4c5bcbc0043a54d
Codeguer <15946817309@163.com>
2021-12-24 22:35:22 +0800

    2021/12/24

---
+++ 计算 1!+2!+3!+……+10!/text.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	int num = 1;
+	int cmt = 1;//cumulant¿€ª˝¡ø
+	int i = 1;
+	for (i = 2; i <=10; i++)
+	{
+		cmt *= i;
+		num += cmt;
+	}
+	printf("%d\n", num);
+	return 0;
+}
